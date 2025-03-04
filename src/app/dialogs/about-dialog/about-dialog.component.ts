fb7a2188604d03417afaeddf1fc35f4442eedda1
Shady <106849270+Jamtart420@users.noreply.github.com>
2025-03-04 02:10:52 -0800

    Update about-dialog.component.ts
    cleaned-up version

--- src/app/dialogs/about-dialog/about-dialog.component.ts
+++ src/app/dialogs/about-dialog/about-dialog.component.ts
@@ -11,21 +11,20 @@ export class AboutDialogComponent implements OnInit {
 
   projectLink = 'https://github.com/Tzahi12345/YoutubeDL-Material';
   issuesLink = 'https://github.com/Tzahi12345/YoutubeDL-Material/issues';
-  latestUpdateLink = 'https://github.com/Tzahi12345/YoutubeDL-Material/releases/latest'
-  latestGithubRelease = null;
-  checking_for_updates = true;
+  latestUpdateLink = 'https://github.com/Tzahi12345/YoutubeDL-Material/releases/latest';
+  latestGithubRelease: any = null;
+  checkingForUpdates = true;
+  currentVersionTag = CURRENT_VERSION;
 
-  current_version_tag = CURRENT_VERSION;
-
-  constructor(public postsService: PostsService) { }
+  constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
     this.getLatestGithubRelease();
   }
 
-  getLatestGithubRelease() {
+  private getLatestGithubRelease(): void {
     this.postsService.getLatestGithubRelease().subscribe(res => {
-      this.checking_for_updates = false;
+      this.checkingForUpdates = false;
       this.latestGithubRelease = res;
     });
   }
