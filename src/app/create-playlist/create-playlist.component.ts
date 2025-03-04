f3e8a4f8b491a71e0221a8cc0a09e546b831b6e6
Shady <106849270+Jamtart420@users.noreply.github.com>
2025-03-04 02:06:01 -0800

    Update create-playlist.component.ts
    cleaned-up version.

--- src/app/create-playlist/create-playlist.component.ts
+++ src/app/create-playlist/create-playlist.component.ts
@@ -10,93 +10,97 @@ import { Playlist } from 'api-types';
   styleUrls: ['./create-playlist.component.scss']
 })
 export class CreatePlaylistComponent implements OnInit {
-  // really "createAndModifyPlaylistDialogComponent"
 
   filesToSelectFrom = null;
-  type = null;
-  filesSelect = new UntypedFormControl();
   audiosToSelectFrom = null;
   videosToSelectFrom = null;
   name = '';
-  cached_thumbnail_url = null;
+  cachedThumbnailUrl: string | null = null;
 
-  create_in_progress = false;
-  create_mode = false;
+  createInProgress = false;
+  createMode = false;
 
-  // playlist modify mode
+  playlist: Playlist | null = null;
+  playlistId: string | null = null;
+  preselectedFiles: any[] = [];
+  playlistUpdated = false;
 
-  playlist: Playlist = null;
-  playlist_id: string = null;
-  preselected_files = [];
-  playlist_updated = false;
+  filesSelect = new UntypedFormControl();
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-              private postsService: PostsService,
-              public dialogRef: MatDialogRef<CreatePlaylistComponent>) {
-                if (this.data?.create_mode) this.create_mode = true;
-                if (this.data?.playlist_id) {
-                  this.playlist_id = this.data.playlist_id;
-                  this.getPlaylist();
-                }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private postsService: PostsService,
+    public dialogRef: MatDialogRef<CreatePlaylistComponent>
+  ) {
+    if (this.data?.create_mode) this.createMode = true;
+    if (this.data?.playlist_id) {
+      this.playlistId = this.data.playlist_id;
+      this.getPlaylist();
+    }
   }
-
 
   ngOnInit(): void {}
 
   createPlaylist(): void {
     const thumbnailURL = this.getThumbnailURL();
-    this.create_in_progress = true;
-    this.postsService.createPlaylist(this.name, this.filesSelect.value, thumbnailURL).subscribe(res => {
-      this.create_in_progress = false;
-      if (res['success']) {
-        this.dialogRef.close(true);
-      } else {
+    this.createInProgress = true;
+    
+    this.postsService.createPlaylist(this.name, this.filesSelect.value, thumbnailURL).subscribe(
+      res => {
+        this.createInProgress = false;
+        this.dialogRef.close(res['success']);
+      },
+      err => {
+        this.createInProgress = false;
+        console.error(err);
         this.dialogRef.close(false);
       }
-    }, err => {
-      this.create_in_progress = false;
-      console.error(err);
-    });
+    );
   }
 
   updatePlaylist(): void {
-    this.create_in_progress = true;
-    this.playlist['name'] = this.name;
-    this.playlist['uids'] = this.filesSelect.value;
-    this.playlist_updated = true;
-    this.postsService.updatePlaylist(this.playlist).subscribe(() => {
-      this.create_in_progress = false;
-      this.postsService.openSnackBar($localize`Playlist updated successfully.`);
-      this.getPlaylist();
-      this.postsService.playlists_changed.next(true);
-    }, err => {
-      this.create_in_progress = false;
-      console.error(err)
-      this.postsService.openSnackBar($localize`Playlist updated successfully.`);
-    });
+    this.createInProgress = true;
+    this.playlist.name = this.name;
+    this.playlist.uids = this.filesSelect.value;
+    this.playlistUpdated = true;
+
+    this.postsService.updatePlaylist(this.playlist).subscribe(
+      () => {
+        this.createInProgress = false;
+        this.postsService.openSnackBar($localize`Playlist updated successfully.`);
+        this.getPlaylist();
+        this.postsService.playlistsChanged.next(true);
+      },
+      err => {
+        this.createInProgress = false;
+        console.error(err);
+        this.postsService.openSnackBar($localize`Playlist update failed.`);
+      }
+    );
   }
 
   getThumbnailURL(): string {
-    return this.cached_thumbnail_url;
+    return this.cachedThumbnailUrl || '';
   }
 
-  fileSelectionChanged({new_selection, thumbnailURL}: {new_selection: string[], thumbnailURL: string}): void {
+  fileSelectionChanged({ new_selection, thumbnailURL }: { new_selection: string[], thumbnailURL: string }): void {
     this.filesSelect.setValue(new_selection);
-    if (new_selection.length) this.cached_thumbnail_url = thumbnailURL;
-    else                      this.cached_thumbnail_url = null;
+    this.cachedThumbnailUrl = new_selection.length ? thumbnailURL : null;
   }
 
   playlistChanged(): boolean {
-    return JSON.stringify(this.playlist.uids) !== JSON.stringify(this.filesSelect.value) || this.name !== this.playlist.name;
+    return JSON.stringify(this.playlist?.uids) !== JSON.stringify(this.filesSelect.value) || this.name !== this.playlist?.name;
   }
 
   getPlaylist(): void {
-    this.postsService.getPlaylist(this.playlist_id, null, true).subscribe(res => {
+    if (!this.playlistId) return;
+
+    this.postsService.getPlaylist(this.playlistId, null, true).subscribe(res => {
       if (res['playlist']) {
         this.filesSelect.setValue(res['file_objs'].map(file => file.uid));
-        this.preselected_files = res['file_objs'];
+        this.preselectedFiles = res['file_objs'];
         this.playlist = res['playlist'];
-        this.name = this.playlist['name']; 
+        this.name = this.playlist.name;
       }
     });
   }
